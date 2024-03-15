67fb74f86d2aaaf7c1b6780bdcc45ab4e8b0bc3a
VikingForcee <119648800+VikingForcee@users.noreply.github.com>
2024-03-15 19:48:36 +0530

    added home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import { Fragment} from 'react'
+import React, { useState, useEffect, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
 
@@ -8,14 +12,22 @@ function App() {
   })
 
   return (
+    <Router>
+      <Header></Header>
     <Fragment>
-      <div classname='container'>
+      <div classname="flex flex-col min-h-screen">
         <h1 className='text-center'>Urban Scan</h1>
         <div style={{ width:"100%",height:"90vh"}}>
-
+          
         </div>
       </div>
     </Fragment>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+      <Footer></Footer>
+
+    </Router>
   )
 }1
 
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white">
+      <h1 className="text-3xl font-bold">Urban Scan</h1>
+      <p className="text-sm">Addressing urban issues, one click at a time</p>
+    </header>
+  );
+}
+
+export default Header;
